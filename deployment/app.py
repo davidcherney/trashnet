258a591d72dee3a821da2beb09fc7e045badf0ce
@Jimpton <dmcherney@gmail.com>
2022-04-16 13:24:59 -0600

    run pred when pi selected

--- deployment/app.py
+++ deployment/app.py
@@ -13,8 +13,9 @@ model = load_model('deployment/model.h5')
 
 if img_file_buffer is not None:
     bytes_data = img_file_buffer.getvalue()
-    # image = tf.io.decode_image(bytes_data, channels=3)
-    image = PIL.Image.open('deployment/pic.png')
+    array = tf.io.decode_image(bytes_data, channels=3)/255 # the is what read_image_p would give out. 
+    pred = model.predict(np.expand_dims(array, axis=0))
+    image = PIL.Image.open('deployment/pic.png') # 
     st.image(image)
 
 

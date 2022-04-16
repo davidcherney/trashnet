fdf8fdee975cfe01e70510344030375808bc3eba
@Jimpton <dmcherney@gmail.com>
2022-04-16 11:51:50 -0600

    idk if comments work in requirements.txt

--- deployment/app.py
+++ deployment/app.py
@@ -1,8 +1,8 @@
-import streamlit as st
+import streamlit as st #streamlit==1.8.1
 # import pickle
 # import PIL
-import tensorflow
-import pandas as pd
+# import tensorflow.keras # tensorflow==2.8.0 
+import pandas as pd #pandas==1.3.5
 
 
 # model = pickle.load(open('model1.p', 'rb')) # meets zsh: illegal hardware instruction 
@@ -13,9 +13,9 @@ import pandas as pd
 # with open('author_pipe.pkl', 'rb') as f:
 #     model = pickle.load(f)
 
-st.title('Who do YOU write like?')
+st.title('hi there')
 
-st.subheader('Jane Austen or Edgar Alan Poe?')
+# st.subheader('Jane Austen or Edgar Alan Poe?')
 
 # txt = st.text_area('Write your prose here')
 

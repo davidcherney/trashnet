4f216f8cf1b3ead384a8a878559ac7fdb6ae5ac8
@Jimpton <dmcherney@gmail.com>
2022-04-16 14:35:56 -0600

    get this mask right

--- deployment/app.py
+++ deployment/app.py
@@ -24,7 +24,7 @@ if img_file_buffer is not None:
     pred = np.reshape(pred, input_shape) # all ready to convert from greyscale to png 
     plt.axis('off')
     plt.imshow(image,alpha=1.)
-    plt.imshow(pred==1,alpha=0.5)
+    # plt.imshow(pred==1,alpha=0.5)
     plt.savefig('mask.png')
     plot = PIL.Image.open('mask.png') # 
     # image = PIL.Image.open('deployment/pic.png') # 
